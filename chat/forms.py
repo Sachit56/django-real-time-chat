d4cd0bee33acd9774f77ffec1503ed9d3a24b0f7
Sachit Khadka <sachitkhadka73@gmail.com>
2023-12-21 19:28:14 +0545

    sign up fixed

--- chat/forms.py
+++ chat/forms.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class SignupForm(UserCreationForm):
     class Meta:
         model=User
-        fields="__all__"
+        fields=['username','password1','password2']
