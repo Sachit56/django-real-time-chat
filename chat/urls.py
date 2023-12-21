6113fae0f93083636d21d26333cc1412557b39e3
Sachit Khadka <sachitkhadka73@gmail.com>
2023-12-21 20:56:44 +0545

    logout added

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name='chat'
 
 urlpatterns = [
     path('',views.ChatView,name='home'),
-    path('signup/',views.SignupView,name='signup')
+    path('signup/',views.SignupView,name='signup'),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout')
 ]
