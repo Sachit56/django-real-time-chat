d1316b6d4941f786ac817d8309e41a254633982c
Sachit Khadka <sachitkhadka73@gmail.com>
2023-12-22 16:42:28 +0545

    chat rooms with superuser

--- room/views.py
+++ room/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from .models import RoomModel
 
 
 # Create your views here.
 @login_required
 def RoomView(request):
-    return render(request,'room/rooms.html')
+    rooms=RoomModel.objects.all()
+    return render(request,'room/rooms.html',{
+        'rooms':rooms
+    })
