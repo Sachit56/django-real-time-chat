2fcd8e715664d3255d5e9bd83abf03ae129e7a31
Sachit Khadka <sachitkhadka73@gmail.com>
2023-12-23 21:25:21 +0545

    consumers

--- room/consumers.py
+++ room/consumers.py
@@ -1,8 +1,9 @@
 import json
 
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websockets import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name=self.scope['url_route']['kwargs']['room_name']
+        self.name=self
